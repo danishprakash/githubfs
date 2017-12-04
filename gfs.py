1ce565cc1be3bdc4d98c35d6bfa89b2a82f781bb
prakashdanish <grafitykoncept@gmail.com>
2017-12-04 22:57:47 +0530

    fu

--- gfs.py
+++ gfs.py
@@ -5,13 +5,12 @@ import sys
 import errno
 import pwd
 
-
 from stat import S_IFDIR, S_IFLNK, S_IFREG, S_ISREG
 from github import Github
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 from time import time, mktime, sleep
 
-class lfs(Operations, LoggingMixIn):
+class lfs(Operations):
 	def __init__(self, root):
 		self.root = root
 		#		self.user = Github(input("Username: "), input("Password: "))
@@ -25,41 +24,35 @@ class lfs(Operations, LoggingMixIn):
 		full_path = self.root + path
 		#print(path)
 		#print('[getattr]')
-		if path == '/repos' or path == '/':
-			st = os.lstat(full_path)
-			return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-				'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-		elif not('.' in path):
-			print('in there')
+		properties = dict(
+				st_mode = S_IFDIR | 755,
+				st_nlink = 2,
+				st_ctime=int(time()),
+				st_mtime=int(time()),
+				st_atime=int(time()),
+				st_uid=pwd.getpwuid(os.getuid()).pw_uid,
+				st_gid=pwd.getpwuid(os.getuid()).pw_gid,
+				)
+		print(path)
+		path_ele = path.split('/')
+		print(path_ele[-1])
+		if path == '/' or path_ele[-1].startswith('.'):
+			pass
+		elif path == '/repos' or path_ele[-1] in self.repo_list:
+			pass
+		elif path_ele[-1] in self.repo_list:
 			properties = dict(
-					st_mode=S_IFDIR | 755,
+					st_mode=S_IFREG | 444,
 					st_size=4096,
-					st_ctime=int(time()),
-					st_mtime=int(time()),
-					st_atime=int(time()),
-					st_uid=pwd.getpwuid(os.getuid()).pw_uid,
-					st_gid=pwd.getpwuid(os.getuid()).pw_gid,
-					st_nlink=0
-					)
-			return properties
-		else:
-			print('in here')
-			properties = dict(
-					st_mode=S_IFREG | 755,
-					st_ctime=int(time()),
-					st_mtime=int(time()),
-					st_atime=int(time()),
-					st_uid=pwd.getpwuid(os.getuid()).pw_uid,
-					st_gid=pwd.getpwuid(os.getuid()).pw_gid,
 					st_nlink=0,
 					)
-			return properties
+		return properties
 	
-#	def open(self, path, flags):
-#		print('[open]')
-#		return 0
+	#def open(self, path, flags):
+	#	print('[open]')
+	#	return 0
 
-	def read(self, path, size, offset, fh=None):
+	def iread(self, path, size, offset, fh=None):
 		file_content = ''
 		path_ele = path.split('/')
 		print('***[read]')
@@ -89,31 +82,24 @@ class lfs(Operations, LoggingMixIn):
 
 	def readdir(self, path, fh):
 		full_path = self.root + path
-		#print(full_path)
 		repo_list = ['.', '..']
-		#if os.path.isdir(full_path):
-		#	dirents.extend(os.listdir(full_path))
-		#for r in dirents:
-		#	yield r
+		path_ele = path.split('/')
 		print('[readdir]')
-		
-		if path == '/':
-			repo_list = ['.', '..', 'repos']
+		if path == '/' or path_ele[-1].startswith('.'):
+			 return ['.', '..', 'repos']
 		elif path == '/repos':
-			#print(self.repo_list)
-			repo_list = repo_list + self.repo_list
-		else:
-			path = path.split('/')
-			repo_name = path[-1]
+			return repo_list + self.repo_list
+		elif path_ele[-1] in self.repo_list:
+			repo_name = path_ele[-1]
+			print(repo_name)
 			for item in self.user.get_user().get_repos():
 				if item.name == repo_name:
 					files = item.get_dir_contents('/')
 					break
 			for item in files:
 				repo_list.append(item.name)
-			#print(repo_list)
-		for item in repo_list:
-			yield item
+			print(repo_list)
+			return repo_list
 
 def main(mountpoint, root):
 	FUSE(lfs(root), mountpoint, nothreads=True, foreground=True)
